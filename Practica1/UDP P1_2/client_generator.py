fa56e88307bd64228911f1c775b93e54d7176829
David Egea Hernández <201805328@alu.comillas.edu>
2022-01-31 11:28:15 +0100

    update gen

--- Practica1/UDP P1_2/client_generator.py
+++ Practica1/UDP P1_2/client_generator.py
@@ -1,9 +1,12 @@
 from client import Client
+import subprocess
 
 """ P1 UDP: Generador de clientes. """
 # Direccion del servidor
 server_ip = "127.0.0.1"
 server_port = 6700
+# Se lanza el servidor
+subprocess.Popen
 # Numero de clientes
 n = 3
 # Se crean clientes recursivamente
@@ -11,5 +14,5 @@ for i in range(0,n):
     # Construccion del mensaje de saludo
     msg = "Hola, soy el cliente " + str(i) 
     client = Client()
-    client.send_to_server(server_ip,server_port,msg)
+    client.send_to_server(server_ip,msg)
     del client
