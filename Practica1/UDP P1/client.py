f503382d110b030ca6907dc539fdfee5d3a8a154
David Egea Hernández <201805328@alu.comillas.edu>
2022-01-31 10:59:15 +0100

    Add client generator and Client class

--- Practica1/UDP P1/client.py
+++ Practica1/UDP P1/client.py
@@ -1,12 +1,25 @@
-import socket 
+import socket
 
-msg = "Hello"
-bytes_tx = str.encode(msg)
+class Client:
 
-server_address = ("127.0.0.1",6700)
-socket = socket.socket(family=socket.AF_INET,type=socket.SOCK_DGRAM)
-socket.sendto(bytes_tx,server_address)
+    def __init__(self) -> None:
+        # Creaccion del socket
+        self.socket = socket.socket(family=socket.AF_INET,type=socket.SOCK_DGRAM)
 
-bytes_rx = socket.recvfrom(1024)
-print(f"RX: {bytes_rx}")
-socket.close()
+    def __del__(self) -> None:
+        # Se cierra el socket
+        socket.close()
+
+    def send_to_server(self, server_ip: str, server_port: int, msg: str="Hello im a client") -> None:
+        # Serializacion del mensaje
+        bytes_tx = str.encode(msg)
+        # Se muestra la respuesta del servidor
+        print(f"[Cliente]: Mensaje enviado al servidor: {bytes_tx}")
+        # Asigna la direccion del servidor
+        server_address = (server_ip,server_port)
+        # Envia el mensaje al servidor
+        socket.sendto(bytes_tx,server_address)
+        # Recibe la respuesta del servidor
+        bytes_rx = socket.recvfrom(1024)
+        # Se muestra la respuesta del servidor
+        print(f"[Cliente]: Respuesta del servidor: {bytes_rx}")
---
+++ Practica1/UDP P1/client_generator.py
@@ -0,0 +1,15 @@
+from client import Client
+
+""" P1 UDP: Generador de clientes. """
+# Direccion del servidor
+server_ip = "127.0.0.1"
+server_port = 6700
+# Numero de clientes
+n = 3
+# Se crean clientes recursivamente
+for i in range(0,n+1):
+    # Construccion del mensaje de saludo
+    msg = "Hello Server, I’m Client " + str(i) 
+    client = Client()
+    client.send(server_ip,server_port,msg)
+    del client
