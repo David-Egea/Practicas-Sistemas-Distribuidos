b5125cf2f40f8911873cb280a3542cd7b7703ef7
rulopelon <48516780+rulopelon@users.noreply.github.com>
2022-01-31 10:33:52 +0100

    arrelgos

--- Practica1/UDP P1/server.py
+++ Practica1/UDP P1/server.py
@@ -3,7 +3,7 @@ import socket
 msg = "Hello Client"
 bytes_tx = str.encode(msg)
 socket = socket.socket(family=socket.AF_INET,type=socket.SOCK_DGRAM)
-socket.bind("127.0.0.1",6700)
+socket.bind(("127.0.0.1",6700))
 
 bytes_rx = socket.recvfrom(1024)
 message = bytes_rx[0]
