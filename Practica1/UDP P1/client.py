48f11e18056e4897ef230524edf33d913c3bbf39
rulopelon <48516780+rulopelon@users.noreply.github.com>
2022-01-31 10:43:41 +0100

    server actualizado

--- Practica1/UDP P1/client.py
+++ Practica1/UDP P1/client.py
@@ -3,7 +3,7 @@ import socket
 msg = "Hello"
 bytes_tx = str.encode(msg)
 
-server_address = ("172.24.155.95",6700)
+server_address = ("127.0.0.1",6700)
 socket = socket.socket(family=socket.AF_INET,type=socket.SOCK_DGRAM)
 socket.sendto(bytes_tx,server_address)
 
--- Practica1/UDP P1/server.py
+++ Practica1/UDP P1/server.py
@@ -1,12 +1,18 @@
 import socket
 
-msg = "Hello Client"
+
+i = 1
 bytes_tx = str.encode(msg)
 socket = socket.socket(family=socket.AF_INET,type=socket.SOCK_DGRAM)
 socket.bind(("127.0.0.1",6700))
 
-bytes_rx = socket.recvfrom(1024)
-message = bytes_rx[0]
-address = bytes_rx[1]
-print(message)
-socket.sendto(bytes_tx,address)
+
+while True:
+    msg = "Hello Client {}".format(i)
+    i +=1
+    bytes_rx = socket.recvfrom(1024)
+    message = bytes_rx[0]
+    address = bytes_rx[1]
+    
+    print(message)
+    socket.sendto(bytes_tx,address)
